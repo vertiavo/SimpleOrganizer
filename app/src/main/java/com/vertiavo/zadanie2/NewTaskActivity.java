b9cd6f91b766f1ea9bf04e19c4128003b20f85c8
vertiavo <vertiavo@gmail.com>
2017-10-04 18:58:54 +0200

    added DatePicker and TimePicker

--- app/src/main/java/com/vertiavo/zadanie2/NewTaskActivity.java
+++ app/src/main/java/com/vertiavo/zadanie2/NewTaskActivity.java
@@ -1,10 +1,14 @@
 package com.vertiavo.zadanie2;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -12,6 +16,9 @@ public class NewTaskActivity extends AppCompatActivity {
 
     private DatePicker datePicker;
     private TextView dateView;
+    private TimePicker timePicker;
+    private TextView time;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +28,19 @@ public class NewTaskActivity extends AppCompatActivity {
         EditText title = (EditText) findViewById(R.id.insert_title);
         EditText subtitle = (EditText) findViewById(R.id.insert_subtitle);
         dateView = (TextView) findViewById(R.id.insert_date_view);
+        timePicker = (TimePicker) findViewById(R.id.insert_time);
+        time = (TextView) findViewById(R.id.insert_time_current);
 
         Calendar calendar = Calendar.getInstance();
-        int year, month, day;
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
 
         showDate(year, month+1, day);
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        showTime(hour, minute);
     }
 
     private void showDate(int year, int month, int day) {
@@ -38,5 +50,49 @@ public class NewTaskActivity extends AppCompatActivity {
                 .append(month)
                 .append("/")
                 .append(year));
+    }
+
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            showDate(year, month+1, day);
+        }
+    };
+
+    private void showTime(int hour, int minute) {
+        String format;
+        if (hour == 0) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+
+        time.setText(new StringBuilder().append(hour).append(" : ").append(minute)
+                .append(" ").append(format));
+    }
+
+    public void setTime(View view) {
+        int hour = timePicker.getCurrentHour();
+        int minute = timePicker.getCurrentMinute();
+        showTime(hour, minute);
     }
 }
